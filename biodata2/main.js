cf33ff8ed8e3c0a0dc2b6cdfea3dcf9c75e99d06
yulianaadhani <142281027+Ylnaadhni@users.noreply.github.com>
2023-12-12 09:32:37 +0700

    Add files via upload

---
+++ biodata2/main.js
@@ -0,0 +1,7 @@
+let nama = "Nama: YULIANA ADHANI";
+let kelas = "Kelas: XI";
+let jurusan = "Jurusan: PPLG";
+let umur = "Umur: 16";
+let alamat = "Alamat: Bojongnangka";
+
+document.write(nama + "<br>", kelas + "<br>", jurusan + "<br>", umur +"<br>", alamat + "<br>");
